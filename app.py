fc377f96a37815443802962dbaaacc709117164f
Rikk <tarikmoussa90@gmial.com>
2026-04-20 16:15:24 -0300

    feat: if/elif/else

--- app.py
+++ app.py
@@ -14,3 +14,12 @@ print("4. Sair\n")
 
 escolha_num = int(input("Escolha uma opcao: "))
 print(f"Você escolheu a opcao: {escolha_num} ")
+
+if escolha_num == 1:
+    print("Cadastrar Restaurante")
+elif escolha_num == 2:
+    print("Listar Restaurantes")
+elif escolha_num == 3:
+    print("Ativar Restaurante")
+else:
+    print("Encerrando o Programa")
