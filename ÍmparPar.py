25e24a853aa8445ae6ae0a8a47ddc1eb8fecb6e1
Rikk <tarikmoussa90@gmial.com>
2026-04-20 18:30:43 -0300

    feat: regras de negocio e estrutura if else para idade

--- ÍmparPar.py
+++ ÍmparPar.py
@@ -1,15 +1,15 @@
 #Input e estrutura ímpar e par
 numI = int(input("Insira um número inteiro: "))
 if numI % 2 == 0:
-    print("O número é par.")
+    print("O número é par.\n")
 else:
-    print("O número é ímpar.")
+    print("O número é ímpar.\n")
 
 #Regras de negócio para idade e input
 idade = int(input("Insira sua idade: "))
 if idade >= 0 and idade <= 13:
-    print("Criança")
+    print("Você é uma criança")
 elif idade >= 13 and idade <= 18:
-    print("Adolescente")
+    print("Você é um adolescente")
 else:
-    print("Adulto")
+    print("Você é um adulto")
