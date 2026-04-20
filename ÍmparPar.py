aac8068f762e8ae3677fd9a852ad5cac055c03cb
Rikk <tarikmoussa90@gmial.com>
2026-04-20 18:25:53 -0300

    feat: regras de negocio e estrutura if else para idade

--- ÍmparPar.py
+++ ÍmparPar.py
@@ -1,3 +1,8 @@
+#Input e estrutura ímpar e par
 numI = int(input("Insira um número inteiro: "))
+if numI % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
 
-if numi
+#Regras de negócio para idade
