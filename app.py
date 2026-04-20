41d1f058a3b9b613ae0c5a6b05a4ba90d1adee0f
Rikk <tarikmoussa90@gmial.com>
2026-04-20 18:17:09 -0300

    feat: def da main

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
-
-print("""
+def exibir_nprograma():
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░
@@ -9,24 +9,32 @@ print("""
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░
 """)
-
-print("1. Cadastrar Restaurante")
-print("2. Listar Restaurantes")
-print("3. Ativar Restaurante")
-print("4. Sair\n")
-
-escolha_num = int(input("Escolha uma opcao: "))
-#print(f"Você escolheu a opcao: {escolha_num} ")
+def exibir_opcoes():
+    print("1. Cadastrar Restaurante")
+    print("2. Listar Restaurantes")
+    print("3. Ativar Restaurante")
+    print("4. Sair\n")
 
 def finalizar_app():
     os.system('cls')
     print('Finalizando o app')
 
-if escolha_num == 1:
-    print("Cadastrar Restaurante")
-elif escolha_num == 2:
-    print("Listar Restaurantes")
-elif escolha_num == 3:
-    print("Ativar Restaurante")
-else:
-    finalizar_app()
+def escolher_opcao():
+    escolha_num = int(input("Escolha uma opcao: "))
+    #print(f"Você escolheu a opcao: {escolha_num} ")
+    if escolha_num == 1:
+        print("Cadastrar Restaurante")
+    elif escolha_num == 2:
+        print("Listar Restaurantes")
+    elif escolha_num == 3:
+        print("Ativar Restaurante")
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nprograma()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
